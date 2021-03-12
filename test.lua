f6d6512c45a5b0cf078e63403e940c08f0004ce0
SAlexW <38391381+SAlexW@users.noreply.github.com>
2021-03-12 11:33:16 +1000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,73 @@
+local comp = require("component")
+local math = require("math")
+local mod = require("modem")
+local event = require("event")
+local pc = require("computer")
 
+function vprojxy (vec)
+  local new = vec
+  new["z"] = 0
+  return new
+  end
+
+function vprojyz (vec)
+  local new = vec
+  new["x"] = 0
+  return new
+  end
+
+function vprojxz (vec)
+  local new = vec
+  new["y"] = 0
+  return new
+  end
+
+function vlen(vec)
+  local new
+  new = math.sqrt(vec["x"]*vec["x"]+vec["y"]*vec["y"]+vec["z"]*vec["z"])
+  return new
+  end
+
+function vcosab (a, b)
+  local cos
+  cos = (a["x"]*b["x"]+a["y"]*b["y"])/(vlen(a)*vlen(b))
+  return cos
+  end
+
+function vsinab (a, b)
+  local sin
+  sin = math.sqrt(1-math.pow(vcosab(a, b), 2))
+  return sin
+  end
+
+function vadd (a, b)
+  local new = a
+  new["x"] = a["x"] + b["x"]
+  new["y"] = a["y"] + b["y"]
+  new["z"] = a["z"] + b["z"]
+  return new
+  end
+
+function vsub (a, b)
+  local new = a
+  new["x"] = a["x"] - b["x"]
+  new["y"] = a["y"] - b["y"]
+  new["z"] = a["z"] - b["z"]
+  return new
+  end
+
+function vmul (vec, num)
+  local new = vec
+  new["x"] = vec["x"] * num
+  new["y"] = vec["y"] * num
+  new["z"] = vec["z"] * num
+  return new
+  end
+
+function vdiv (vec, num)
+  local new = vec
+  new["x"] = vec["x"] / num
+  new["y"] = vec["y"] / num
+  new["z"] = vec["z"] / num
+  return new
+  end
